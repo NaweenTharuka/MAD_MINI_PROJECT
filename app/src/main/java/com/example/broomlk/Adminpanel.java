1a0d651a7b959365c1c73d551f3dc7b84131fd47
NaweenTharuka <61498921+NaweenTharuka@users.noreply.github.com>
2020-04-12 22:25:43 +0530

    broom

---
+++ app/src/main/java/com/example/broomlk/Adminpanel.java
@@ -0,0 +1,30 @@
+package com.example.broomlk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Adminpanel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adminpanel);
+
+        Button adddriver = (Button)findViewById(R.id.driver);
+        Button addvehicle = (Button)findViewById(R.id.vehicle);
+
+        addvehicle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent continueIntent = new Intent(Adminpanel.this,AdminView.class);
+                startActivity(continueIntent);
+            }
+        });
+
+
+    }
+}
